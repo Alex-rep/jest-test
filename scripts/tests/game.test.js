9795ab64cb2ba5ce3a7456dc8167472de046977f
Alex-rep <nuclearCMP_t1058@protonmail.com>
2022-10-26 14:06:51 +0000

    Add finishing touches

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,8 +1,9 @@
 /**
  * @jest-environment jsdom
  */
- const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
+ const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
 
+jest.spyOn(window, "alert").mockImplementation(() => {});
 
  beforeAll(() => {
      let fs = require("fs");
@@ -36,6 +37,14 @@
 
      test("choices contains the correct IDs", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
+
+     test("turnInProgress exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+     });
+
+     test("lastButton exists", () => {
+        expect("lastButton" in game).toBe(true);
      });
 
  });
@@ -94,5 +103,25 @@
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
-    })
+    });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
+    });
+    test("should toggle turnInProgress to true", () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during the computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
+    });
  });
